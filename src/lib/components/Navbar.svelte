2f0373a3c314bb9843b6fe9e54c4844f8ce0476b
elijaholmos <eli.olmos9@gmail.com>
2023-01-25 21:28:23 -0700

    feat: add `/professor` page w auth header

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+	import LoginHeader from './LoginHeader.svelte';
+</script>
+
+<nav id="header" class="fixed w-full z-30 top-0 text-white">
+	<div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+		<div class="pl-4 flex items-center">
+			<a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+				<img class="h-14 fill-current inline" src="/images/hero.svg" />
+			</a>
+		</div>
+		<LoginHeader />
+	</div>
+	<hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
+</nav>
